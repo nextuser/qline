465e74951f71d1f0b05aec24d48c929626ddfc58
lijinglin <nextuser@163.com>
2025-11-22 09:31:28 +0800

    add chinese description for sub command

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const listNewWord = require('../commands/listNewWord');
 // 配置版本和帮助信息
 program
   .version(version, '-v, --version')
-  .description('基于 SQLite + ECDICT 的命令行中英文词典工具');
+  .description('英中词典：支持查单词 背单词');
 
 // 1. 查询单词：cdict <word>（作为子命令）
 program
@@ -24,6 +24,7 @@ program
 // 2. qline r/record [word]
 program
   .command('save [word]')
+  .description('保存生词')
   .alias('s')
   .action(async (word) => {
       const saveNewWord = require('../commands/saveNewWord');
@@ -33,6 +34,7 @@ program
 // 3. 复习生词：qline r/recall
 program.command('recall')
   .alias('r')
+  .description('复习生词')
   .action(async () => {
      const recallNewWord = require('../commands/recallNewWord');
      await recallNewWord();
@@ -44,6 +46,7 @@ program.command('recall')
 program
   .command('list [count]')
   .alias('l')
+  .description('查看生词列表')
   .action(async (count) => {
     const listNewWord = require('../commands/listNewWord');
     await listNewWord(count);
@@ -53,6 +56,7 @@ program
 program
   .command('delete <word>')
   .alias('d')
+  .description('删除生词')
   .action(async (word) => {
     const deleteNewWord = require('../commands/deleteNewWord');
     await deleteNewWord(word);
