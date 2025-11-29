a4580938e4510e1221b0f7d5a7c52604c39524db
lijinglin <nextuser@163.com>
2025-11-29 18:04:49 +0800

    add zk

--- commands/example.js
+++ commands/example.js
@@ -19,11 +19,11 @@ async function showExample(){
     #清空生词本
     qline c
 
-    # 随机抽取20个考研词汇反复练习  -tag 指定单词标签（ky:考研, cet4:四级, cet6:六级, gk:高考, toffel:托福, ielts:雅思） -c 练习单词个数，缺省是10个
+    # 随机抽取20个考研词汇反复练习  -tag 指定单词标签（zk:中考,gk:高考,ky:考研,cet4:四级, cet6:六级,toffel:托福,ielts:雅思） -c 练习单词个数，缺省是10个
     qline t -tag ky -c 20
-    # 随机抽取20个cet4词汇反复练习 -tag 指定单词标签（ky:考研, cet4:四级, cet6:六级, gk:高考, toffel:托福, ielts:雅思） -c 练习单词个数，缺省是10个
+    # 随机抽取20个cet4词汇反复练习 -tag 指定单词标签（zk:中考,gk:高考,ky:考研,cet4:四级, cet6:六级,toffel:托福,ielts:雅思） -c 练习单词个数，缺省是10个
     qline t -tag cet4 -c 20
-    # 随机抽取10个托福词汇反复练习 -tag 指定单词标签（ky:考研, cet4:四级, cet6:六级, gk:高考, toffel:托福, ielts:雅思）-c 练习单词个数，缺省是10个
+    # 随机抽取10个托福词汇反复练习 -tag 指定单词标签（zk:中考,gk:高考,ky:考研,cet4:四级, cet6:六级,toffel:托福,ielts:雅思）-c 练习单词个数，缺省是10个
     qlin t -tag toefl
     `
     let msg = ""
